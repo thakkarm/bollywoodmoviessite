514f468707b37bdf0ecd932d739a07d67d9ae737
thakkm <thakkm@1a18f56c-916e-6770-049a-77de2fa23164>
2014-02-13 02:19:36 +0000

    Jan 2014

--- trunk/02_bollywoodmovies/scripts/hollywood/celebrites/_NOW.bat
+++ trunk/02_bollywoodmovies/scripts/hollywood/celebrites/_NOW.bat
@@ -7,6 +7,6 @@ REM ----------------------------------------------------------------------------
 REM Need to convert the movie photo template to celebrites photo tempate
 REM -----------------------------------------------------------------------------------------
 REM F:\__site\tools\ssed.exe -f to_celebrites.sed template_photo_gallery_page.html > template_celebrites_photo_gallery_page.html
-F:\__site\tools\ssed.exe -f to_celebrites.sed template_photo_gallery_page_compressed.html > template_celebrites_photo_gallery_page.html
+C:\02_Sites\tools\ssed.exe -f to_celebrites.sed template_photo_gallery_page_compressed.html > template_celebrites_photo_gallery_page.html
 
 call create_celebrites_star_pages.bat celebrites alex_morgan
